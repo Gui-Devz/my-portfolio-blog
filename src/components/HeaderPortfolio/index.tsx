cde2fe9e845b41b9c635140c83b3e8403ccb5fcd
Guilherme <guilherme_devz@hotmail.com>
2021-08-30 17:13:58 -0300

    UPDATE: adding the Modal from 'react-moda' and creating the styling

--- src/components/HeaderPortfolio/index.tsx
+++ src/components/HeaderPortfolio/index.tsx
@@ -1,32 +1,54 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
+import Modal from "react-modal";
 
 import { GoThreeBars } from "react-icons/go";
 import logoImg from "../../assets/logo.svg";
+import { ModalMobileMenu } from "../ModalMobileMenu";
 
 import styles from "./header.module.scss";
 
-export function HeaderPortfolio() {
-  return (
-    <header className={styles.navHeader}>
-      <div className={styles.headerContainer}>
-        <Image src={logoImg} alt="Logo image" />
+Modal.setAppElement("#__next");
 
-        <nav>
-          <Link href="/">
-            <a>Blog</a>
-          </Link>
-          <Link href="#projects">
-            <a>Projects</a>
-          </Link>
-          <Link href="#contactme">
-            <a>Contact me</a>
-          </Link>
-        </nav>
-        <button>
-          <GoThreeBars color="white" size="2em" />
-        </button>
-      </div>
-    </header>
+export function HeaderPortfolio() {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  function toggleModal() {
+    setModalIsOpen(!modalIsOpen);
+  }
+
+  return (
+    <>
+      <header className={styles.navHeader}>
+        <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={toggleModal}
+          contentLabel="Example Modal"
+          className={styles.Modal}
+          overlayClassName={styles.Overlay}
+        >
+          <ModalMobileMenu toggleModal={toggleModal} />
+        </Modal>
+
+        <div className={styles.headerContainer}>
+          <Image src={logoImg} alt="Logo image" />
+          <nav>
+            <Link href="/">
+              <a>Blog</a>
+            </Link>
+            <Link href="#projects">
+              <a>Projects</a>
+            </Link>
+            <Link href="#contactme">
+              <a>Contact me</a>
+            </Link>
+          </nav>
+          <button onClick={() => toggleModal()} className={styles.mobileMenu}>
+            <GoThreeBars color="white" size="2em" />
+          </button>
+        </div>
+      </header>
+    </>
   );
 }
