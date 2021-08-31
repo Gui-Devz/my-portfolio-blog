bd5f2a2912e84b791aa95d41fb43bf2499285ff0
Guilherme <guilherme_devz@hotmail.com>
2021-08-31 17:08:54 -0300

    UPDATE: adding the component 'MyResume' to the document tree

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { useEffect, useState } from "react";
 
 import { Technologies } from "../components/Technologies";
 import Presentation from "../components/Presentation";
@@ -8,10 +9,9 @@ import { MyProjects } from "../components/MyProjects";
 import { MyResume } from "../components/MyResume";
 import { ContactMe } from "../components/ContactMe";
 import { Footer } from "../components/Footer";
+import { ScrollUp } from "../components/ScrollUp";
 
 import styles from "./home.module.scss";
-import { useEffect, useState } from "react";
-import { ScrollUp } from "../components/ScrollUp";
 
 const Home: NextPage = () => {
   const [lastKnownScrollPosition, setLastKnownScrollPosition] = useState(0);
