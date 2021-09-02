d933c273c91cafcb1bdc2606c364eb632728305a
Guilherme <guilherme_devz@hotmail.com>
2021-09-02 20:42:23 -0300

    ADD: creating the Blog page

---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,13 @@
+import { Header } from "../../components/Blog/Header";
+import { Posts } from "../../components/Blog/Posts";
+import { Footer } from "../../components/Footer";
+
+export default function Blog() {
+  return (
+    <>
+      <Header />
+      <Posts />
+      <Footer />
+    </>
+  );
+}
