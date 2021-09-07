a049fa1cae5557ab9c41803bbe265b5bd9def16b
Guilherme <guilherme_devz@hotmail.com>
2021-09-07 00:37:02 -0300

    UPDATE: changing the fav icon image

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,7 +11,11 @@ export default class MyDocument extends Document {
             rel="stylesheet"
           />
 
-          <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+          <link
+            rel="shortcut icon"
+            href="https://user-images.githubusercontent.com/62578862/132279264-5274ab5f-c6d8-475d-8928-2659c864478b.png"
+            type="image/png"
+          />
         </Head>
         <body>
           <Main />
