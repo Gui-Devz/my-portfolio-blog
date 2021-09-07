47d8f325e568c9611795f561fa5d2b816066ad7f
Guilherme <guilherme_devz@hotmail.com>
2021-09-07 00:40:21 -0300

    UPDATE: pulling the data of the posts from Contenful using the CDA

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,13 +1,63 @@
+import Head from "next/head";
+import { GetStaticProps } from "next";
 import { Header } from "../../components/Blog/Header";
 import { Posts } from "../../components/Blog/Posts";
 import { Footer } from "../../components/Footer";
 
-export default function Blog() {
+import { contentfulClient } from "../../services/contentful";
+import { formattingDate } from "../../lib/utils";
+
+import { ContentfulCollection } from "contentful";
+
+type Post = {
+  slug: string;
+  title: string;
+  author: string;
+  heroImgURL: string;
+  publishDate: string;
+  excerpt: string;
+};
+
+interface BlogProps {
+  posts: Post[];
+}
+
+export default function Blog({ posts }: BlogProps) {
   return (
     <>
+      <Head>
+        <title>Blog - Posts about programming</title>
+      </Head>
       <Header />
-      <Posts />
+      <Posts posts={posts} />
       <Footer />
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response: ContentfulCollection<any> = await contentfulClient.getEntries(
+    {
+      limit: 1,
+      content_type: "posts",
+    }
+  );
+
+  const posts = response.items.map((post) => {
+    return {
+      slug: post.fields.slug,
+      title: post.fields.title,
+      author: post.fields.author,
+      heroImgURL: post.fields.heroImage.fields.file.url.replace(/^/, "https:"),
+      publishDate: formattingDate(post.fields.publishDate),
+      excerpt: post.fields.excerpt,
+    };
+  });
+
+  //console.log(posts);
+
+  return {
+    props: { posts },
+    revalidate: 60 * 30,
+  };
+};
