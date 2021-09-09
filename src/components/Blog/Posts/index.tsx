d77a633ee4c7d86d23fbf5e2256bd75b3a7714e0
Guilherme <guilherme_devz@hotmail.com>
2021-09-09 17:26:29 -0300

    UPDATE: implementing the component InfiniteScroll to load the posts and checking if the react query is loading before populate state newPosts inside the useEffect.

--- src/components/Blog/Posts/index.tsx
+++ src/components/Blog/Posts/index.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 import { useInfiniteQuery } from "react-query";
 import { api } from "../../../services/api";
 import { PostCard } from "../PostCard";
@@ -40,33 +41,75 @@ export function Posts({ posts, titleFilter, tagFilter }: PostsProps) {
     return response.data;
   }
 
-  const { data, error, fetchNextPage, hasNextPage, isError } = useInfiniteQuery(
-    "posts",
-    getAllPosts,
-    {
+  const { data, error, fetchNextPage, hasNextPage, isError, isLoading } =
+    useInfiniteQuery("posts", getAllPosts, {
       getNextPageParam: (lastPage) => lastPage?.nextGroup,
-    }
-  );
+    });
 
   useEffect(() => {
     if (posts.length > 0) {
       setNewPosts(posts);
       return;
     }
+    if (!isLoading) {
+      const posts = data.pages.map((page) => {
+        const post = page.posts.map((post) => post);
+        return post;
+      });
 
-    setNewPosts(data.pages[0].posts);
-  }, [posts, data]);
+      setNewPosts(posts.flat());
+    }
+    return;
+  }, [posts, data, isLoading]);
 
   return (
     <div className={styles.postsContainer}>
-      {newPosts &&
+      {titleFilter !== "" && (
+        <p className={styles.filter}>
+          <i>&#34;Filtered by: {titleFilter}&#34;</i>
+        </p>
+      )}
+      {tagFilter !== "" && (
+        <p className={styles.filter}>
+          {" "}
+          <i>&#34;Filtered by: {tagFilter}&#34;</i>
+        </p>
+      )}
+
+      {posts.length === 0 && tagFilter === "" && titleFilter === "" ? (
+        <InfiniteScroll
+          dataLength={!isLoading ? data.pages.length : newPosts.length} //This is important field to render the next data
+          next={fetchNextPage}
+          hasMore={hasNextPage}
+          loader={<h4>Loading...</h4>}
+          endMessage={
+            <p style={{ textAlign: "center" }}>
+              <b>Yay! You have seen it all</b>
+            </p>
+          }
+        >
+          {newPosts &&
+            newPosts.map((post) => {
+              return (
+                <div key={post.slug} className={styles.card}>
+                  <PostCard post={post} />
+                </div>
+              );
+            })}
+        </InfiniteScroll>
+      ) : posts.length === 0 && (tagFilter !== "" || titleFilter !== "") ? (
+        <p className={styles.postNotFound}>
+          Sorry! We couldn&#39;t find any post with the filter selected.
+        </p>
+      ) : (
         newPosts.map((post) => {
           return (
             <div key={post.slug} className={styles.card}>
               <PostCard post={post} />
             </div>
           );
-        })}
+        })
+      )}
     </div>
   );
 }
