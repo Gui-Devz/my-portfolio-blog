27470322d3b80ae7904d00772bab379eabb091f6
Guilherme <guilherme_devz@hotmail.com>
2021-09-08 21:16:18 -0300

    UPDATE: setting the useEffect to setNewPosts in case of either filters applied or by the data from react-query

--- src/components/Blog/Posts/index.tsx
+++ src/components/Blog/Posts/index.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useInfiniteQuery } from "react-query";
+import { api } from "../../../services/api";
 import { PostCard } from "../PostCard";
 
 import styles from "./posts.module.scss";
@@ -12,15 +14,53 @@ type Post = {
   excerpt: string;
 };
 
+type ResponseValue = {
+  data: {
+    posts: Post[];
+    nextGroup?: string;
+  };
+};
+
 interface PostsProps {
   posts: Post[];
+  titleFilter: string;
+  tagFilter: string;
 }
 
-export function Posts({ posts }: PostsProps) {
+export function Posts({ posts, titleFilter, tagFilter }: PostsProps) {
+  const [newPosts, setNewPosts] = useState<Post[]>([]);
+
+  async function getAllPosts({ pageParam = 0 }) {
+    const response: ResponseValue = await api.get("posts", {
+      params: {
+        nextGroup: pageParam,
+      },
+    });
+
+    return response.data;
+  }
+
+  const { data, error, fetchNextPage, hasNextPage, isError } = useInfiniteQuery(
+    "posts",
+    getAllPosts,
+    {
+      getNextPageParam: (lastPage) => lastPage?.nextGroup,
+    }
+  );
+
+  useEffect(() => {
+    if (posts.length > 0) {
+      setNewPosts(posts);
+      return;
+    }
+
+    setNewPosts(data.pages[0].posts);
+  }, [posts, data]);
+
   return (
     <div className={styles.postsContainer}>
-      {posts &&
-        posts.map((post) => {
+      {newPosts &&
+        newPosts.map((post) => {
           return (
             <div key={post.slug} className={styles.card}>
               <PostCard post={post} />
