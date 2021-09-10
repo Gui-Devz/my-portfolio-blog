a921428837bef11f4146d3681ed9d8090cf1bb7e
Guilherme <guilherme_devz@hotmail.com>
2021-09-09 22:30:04 -0300

    ADD: creating the component 'Social' and its style sheet for the blog.

---
+++ src/components/Blog/Social/index.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import { AiFillLinkedin } from "react-icons/ai";
+import { AiOutlineGithub } from "react-icons/ai";
+import { FiTwitter } from "react-icons/fi";
+
+import styles from "./social.module.scss";
+
+export function Social() {
+  return (
+    <div className={styles.socialWrapper}>
+      <Link href="https://www.linkedin.com/in/guilherme-batalha-2b913448/">
+        <a target="_blank">
+          <AiFillLinkedin fontSize="2em" />
+        </a>
+      </Link>
+      <Link href="https://www.github.com/Gui-Devz">
+        <a target="_blank">
+          <AiOutlineGithub fontSize="2em" />
+        </a>
+      </Link>
+      <Link href="https://twitter.com/Batalha97">
+        <a target="_blank">
+          <FiTwitter fontSize="2em" />
+        </a>
+      </Link>
+    </div>
+  );
+}
