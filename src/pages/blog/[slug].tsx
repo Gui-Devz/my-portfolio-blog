4b12994568a5c590edc959ce20896c49aac7cc2e
Guilherme <guilherme_devz@hotmail.com>
2021-09-14 00:03:15 -0300

    UPDATE: changing the logic of how the content retrieved from contentful is rendered and adding the components 'Share' and 'ScrollUp' to the tree

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,17 +1,22 @@
-import { useEffect } from "react";
 import { GetStaticProps } from "next";
 import Head from "next/head";
+
 import { contentfulClient } from "../../services/contentful";
 import { ContentfulCollection } from "contentful";
 
-import { BLOCKS, MARKS } from "@contentful/rich-text-types";
-import { documentToHtmlString } from "@contentful/rich-text-html-renderer";
+import { Document } from "@contentful/rich-text-types";
 
 import { HeaderPost } from "../../components/Blog/Header/HeaderPost";
+import { PostDebrief } from "../../components/Blog/PostDebrief";
+import { PostContent } from "../../components/Blog/PostContent";
+import { Footer } from "../../components/Footer";
 
 import { formattingDate } from "../../lib/utils";
 
 import styles from "./post.module.scss";
+import { Share } from "../../components/Blog/Share";
+import React from "react";
+import { ScrollUp } from "../../components/ScrollUp";
 
 type Post = {
   tags: string[];
@@ -19,62 +24,35 @@ type Post = {
   author: string;
   heroImgURL: string;
   updateDate: string;
-  content: string;
+  content: Document;
 };
 
 interface PostProps {
   post: Post;
 }
 export default function Post({ post }: PostProps) {
-  //console.log(Math.ceil(post.content.length / 1000));
-  let readingTime = 0;
-
-  /* function calculateReadingTime(): void {
-    // const filter = /(\s*\w*)/g;
-    const totalWords = post.content.reduce(
-      (acc, content) => {
-        const headingWords = content.heading.split(" ");
-        const bodyWords = content.body.map((paragraph) => {
-          return paragraph.text.split(" ");
-        });
-        let total = headingWords.length;
-        bodyWords.forEach((wordsArray) => {
-          total += wordsArray.length;
-        });
-        acc.words += total;
-        // console.log(total);
-        return acc;
-      },
-      { words: 0 }
-    );
-
-    readingTime = Math.ceil(totalWords.words / 200);
-    // console.log(totalWords);
-  }
-
-   useEffect(() => {
-  calculateReadingTime();
-   }, []); */
-
   return (
     <>
       <Head>
         <title>Blog - {post.title}</title>
       </Head>
       <HeaderPost />
-      <div className={styles.container}>
-        <section
-          className={styles.post}
-          dangerouslySetInnerHTML={{ __html: post.content }}
-        ></section>
-      </div>
+      <PostDebrief
+        tags={post.tags}
+        title={post.title}
+        heroImg={post.heroImgURL}
+        updatedAt={post.updateDate}
+        author={post.author}
+      />
+      <PostContent content={post.content} />
+      <Share />
+      <ScrollUp />
+      <Footer />
     </>
   );
 }
 
 export async function getStaticPaths({ params }) {
-  console.log();
-
   return {
     paths: [],
     fallback: "blocking",
@@ -86,28 +64,12 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   const response: ContentfulCollection<any> = await contentfulClient.getEntries(
     {
       content_type: "posts",
-      "fields.slug": slug,
+      "fields.slug[in]": slug,
     }
   );
 
   const responsePost = response.items[0];
   const updateDate = formattingDate(responsePost.sys.updatedAt);
-
-  const options = {
-    renderMark: {
-      [MARKS.CODE]: (text) => {
-        return `<code>${text}</code>`;
-      },
-    },
-    /* renderNode: {
-      [BLOCKS.EMBEDDED_ASSET]: (node) =>
-        `<img src="${node.data.target.fields.file.url.replace(
-          /^/,
-          "https:"
-        )}" alt="${node.data.target.fields.description}"/>`,
-    }, */
-  };
-  const content = documentToHtmlString(responsePost.fields.content, options);
 
   const post = {
     tags: responsePost.metadata.tags.map((tag) => tag.sys.id),
@@ -117,9 +79,8 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
       /^/,
       "https:"
     ),
+    content: responsePost.fields.content,
     updateDate,
-    content,
-    obj: responsePost.fields.content,
   };
   return {
     props: {
