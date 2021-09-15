b85678b2acd7b4c7fe2df8240ee7c7ca7ef7471e
Guilherme <guilherme_devz@hotmail.com>
2021-09-15 17:47:24 -0300

    UPDATE: configuring the page to receive the prop data from static generation(fetching from contentful) and passing it to the child components.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
-import type { NextPage } from "next";
+import type { GetStaticProps } from "next";
 import Head from "next/head";
-import { useEffect, useState } from "react";
+
+import { contentfulClient } from "../services/contentful";
+import { ContentfulCollection } from "contentful";
 
 import { Technologies } from "../components/Technologies";
 import Presentation from "../components/Presentation";
@@ -13,7 +15,29 @@ import { ScrollUp } from "../components/ScrollUp";
 
 import styles from "./home.module.scss";
 
-const Home: NextPage = () => {
+type EntryResume = {
+  fields: {
+    resumeLink: string;
+  };
+};
+
+type Project = {
+  title: string;
+  content: string;
+  codeLink: string;
+  liveDemoLink: string;
+  imageURL: string;
+  imageDescription: string;
+};
+
+interface HomeProps {
+  data: {
+    projects: Project[];
+    myResume: string;
+  };
+}
+
+export default function Home({ data }: HomeProps) {
   return (
     <>
       <div className={styles.main}>
@@ -23,14 +47,44 @@ const Home: NextPage = () => {
         <HeaderPortfolio />
         <Presentation />
         <Technologies />
-        <MyProjects />
-        <MyResume />
+        <MyProjects projects={data.projects} />
+        <MyResume resumeLink={data.myResume} />
         <ContactMe />
         <Footer />
         <ScrollUp />
       </div>
     </>
   );
-};
+}
 
-export default Home;
+export const getStaticProps: GetStaticProps = async () => {
+  const responseProjects: ContentfulCollection<any> =
+    await contentfulClient.getEntries({
+      content_type: "projects",
+    });
+
+  const responseMyResume: EntryResume = await contentfulClient.getEntry(
+    "3V8NYbQNQFeeJDB0cqFNlA"
+  );
+
+  const projects = responseProjects.items.map((project) => {
+    return {
+      title: project.fields.title,
+      content: project.fields.content,
+      codeLink: project.fields.codeLink ? project.fields.codeLink : "",
+      liveDemoLink: project.fields.liveDemoLink
+        ? project.fields.liveDemoLink
+        : "",
+      imageURL: "https:" + project.fields.projectImage.fields.file.url,
+      imageDescription: project.fields.projectImage.fields.description,
+    };
+  });
+
+  const myResume = responseMyResume.fields.resumeLink;
+
+  return {
+    props: {
+      data: { projects, myResume },
+    },
+  };
+};
