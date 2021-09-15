1b6ff74dab9fd829a7edf3d5a2658d9ebbd7457c
Guilherme <guilherme_devz@hotmail.com>
2021-09-15 17:38:25 -0300

    UPDATE: re-structuring the footer elements and its stylesheet

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,21 +1,22 @@
 import { Social } from "../Social";
 
-import { MdCopyright } from "react-icons/md";
-
 import styles from "./footer.module.scss";
 export function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.footerContainer}>
-        <div>
-          <p>
-            <MdCopyright /> 2021 Guilherme Zagari
-          </p>
-        </div>
-        <div>
+        <div className={styles.follow}>
           <span>Follow me</span>
           <Social />
         </div>
+        <div className={styles.love}>
+          <p>Made with &#9825;</p>
+        </div>
+      </div>
+      <div className={styles.copy}>
+        <p className={styles.copyRight}>
+          <span>&copy; 2021 Guilherme Zagari</span>
+        </p>
       </div>
     </footer>
   );
