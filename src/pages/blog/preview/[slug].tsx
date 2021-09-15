99f55b530138bb2c672d70affb1a04d521894fc4
Guilherme <guilherme_devz@hotmail.com>
2021-09-15 20:36:22 -0300

    UPDATE: changing the name of the page param of id to slug

---
+++ src/pages/blog/preview/[slug].tsx
@@ -0,0 +1,79 @@
+import React from "react";
+import { GetServerSideProps } from "next";
+import Head from "next/head";
+
+import { previewClient } from "../../../services/contentful";
+import { ContentfulCollection } from "contentful";
+import { Document } from "@contentful/rich-text-types";
+
+import { HeaderPost } from "../../../components/Blog/Header/HeaderPost";
+import { PostDebrief } from "../../../components/Blog/PostDebrief";
+import { PostContent } from "../../../components/Blog/PostContent";
+import { Footer } from "../../../components/Footer";
+import { Share } from "../../../components/Blog/Share";
+import { ScrollUp } from "../../../components/ScrollUp";
+
+import { formattingDate } from "../../../lib/utils";
+
+type Post = {
+  tags: string[];
+  title: string;
+  author: string;
+  heroImgURL: string;
+  updateDate: string;
+  content: Document;
+};
+
+interface PostProps {
+  post: Post;
+}
+export default function PostPreview({ post }: PostProps) {
+  return (
+    <>
+      <Head>
+        <title>Blog preview - {post.title}</title>
+      </Head>
+      <HeaderPost />
+      <PostDebrief
+        tags={post.tags}
+        title={post.title}
+        heroImg={post.heroImgURL}
+        updatedAt={post.updateDate}
+        author={post.author}
+      />
+      <PostContent content={post.content} />
+      <Share />
+      <ScrollUp />
+      <Footer />
+    </>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const { slug } = params;
+
+  const response: ContentfulCollection<any> = await previewClient.getEntries({
+    content_type: "posts",
+    "fields.slug[match]": `"${slug}""`,
+  });
+
+  const responsePost = response.items[0];
+  const updateDate = formattingDate(responsePost.sys.updatedAt);
+
+  const post = {
+    tags: responsePost.metadata.tags.map((tag) => tag.sys.id),
+    title: responsePost.fields.title,
+    author: responsePost.fields.author,
+    heroImgURL: responsePost.fields.heroImage.fields.file.url.replace(
+      /^/,
+      "https:"
+    ),
+    content: responsePost.fields.content,
+    updateDate,
+  };
+  return {
+    props: {
+      post: post,
+    },
+  };
+};
