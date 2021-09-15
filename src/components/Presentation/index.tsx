9ad3bc2a281e5c52c46fce7e164ddb139fc5561c
Guilherme <guilherme_devz@hotmail.com>
2021-09-15 20:28:54 -0300

    UPDATE: changing a 'lil bit the text in the 'presentationContent' div

--- src/components/Presentation/index.tsx
+++ src/components/Presentation/index.tsx
@@ -24,8 +24,8 @@ const Presentation: NextPage = () => {
       </div>
       <div className={styles.presentationContent}>
         <p>
-          Hi! my name is Guilherme, I&apos;m a Jr. dev, I&apos;m always trying
-          to create and learn something new through coding.
+          Hey there! my name is Guilherme, I&apos;m a Jr. dev, I&apos;m always
+          trying to create and learn something new through coding.
         </p>
       </div>
     </section>
