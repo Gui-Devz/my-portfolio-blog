7c7207bd7ebaaa4dcfb878dd63e035ba799051c7
Guilherme <guilherme_devz@hotmail.com>
2021-09-15 17:49:24 -0300

    UPDATE: adding the config client to the contentful preview API

--- src/services/contentful.ts
+++ src/services/contentful.ts
@@ -5,3 +5,10 @@ export const contentfulClient = contentful.createClient({
   environment: process.env.NEXT_PUBLIC_CONTENT_ENVIRONMENT_TOKEN,
   accessToken: process.env.NEXT_PUBLIC_CONTENT_DELIVERY_TOKEN,
 });
+
+export const previewClient = contentful.createClient({
+  host: "preview.contentful.com",
+  space: process.env.NEXT_PUBLIC_CONTENT_SPACE_ID,
+  environment: process.env.NEXT_PUBLIC_CONTENT_ENVIRONMENT_TOKEN,
+  accessToken: process.env.NEXT_PUBLIC_CONTENT_PREVIEW_TOKEN,
+});
