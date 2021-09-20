b86ed666d7d32553b4f157abe135a5b6ed31fc0b
Guilherme <guilherme_devz@hotmail.com>
2021-09-19 21:35:52 -0300

    UPDATE: adjusting the type of the prop 'resumes' and adding the logic for selecting the language version of the resume.

--- src/components/MyResume/index.tsx
+++ src/components/MyResume/index.tsx
@@ -1,29 +1,54 @@
 import Link from "next/link";
+import { useState } from "react";
 import { IoIosPaper } from "@react-icons/all-files/io/IoIosPaper";
 
 import styles from "./myresume.module.scss";
 
+type EntryResume = {
+  language: string;
+  link: string;
+};
+
 interface MyResumeProps {
-  resumeLink: string;
+  resumes: EntryResume[];
 }
 
-export function MyResume({ resumeLink }: MyResumeProps) {
+export function MyResume({ resumes }: MyResumeProps) {
+  const [link, setLink] = useState(resumes[0].link);
   return (
-    <>
+    <section className={styles.resumeSection}>
       <h2 id="resume" className={styles.H2}>
         Resume
       </h2>
-      <section className={styles.resume}>
+      <div className={styles.resume}>
         <h3 className={styles.resumeIntro}>Take a look at my resume:</h3>
 
         <div className={styles.content}>
           <div className={styles.btn}>
-            <Link href={resumeLink}>
+            <Link href={link}>
               <a target="_blank">
                 My resume
                 <IoIosPaper />
               </a>
             </Link>
+            <div>
+              <label htmlFor="language">Select language version:</label>
+              <select
+                name="languages"
+                id="language"
+                value={link}
+                onChange={(e) => setLink(e.target.value)}
+              >
+                {resumes &&
+                  resumes.map((resume) => {
+                    return (
+                      <option key={resume.language} value={resume.link}>
+                        {resume.language}
+                      </option>
+                    );
+                  })}
+              </select>
+            </div>
           </div>
           <div className={styles.warning}>
             <p>
@@ -32,7 +57,7 @@ export function MyResume({ resumeLink }: MyResumeProps) {
             <p>The file is hosted in google drive.</p>
           </div>
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 }
