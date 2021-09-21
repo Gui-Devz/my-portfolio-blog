731e9d535c1e0f0021653754404e57689491e0aa
Guilherme <guilherme_devz@hotmail.com>
2021-09-20 22:03:44 -0300

    UPDATE: changing the domain name

--- src/middlewares/protectAPI.ts
+++ src/middlewares/protectAPI.ts
@@ -2,7 +2,9 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 const protectAPI = (handler) => {
   return async (req: NextApiRequest, res: NextApiResponse) => {
-    if (new URL(req.headers.referer).origin !== "http://localhost:3000") {
+    if (
+      new URL(req.headers.referer).origin !== "https://guizagari.vercel.app/"
+    ) {
       return res.status(403).json({ success: false, message: `Forbidden` });
     }
     return handler(req, res);
