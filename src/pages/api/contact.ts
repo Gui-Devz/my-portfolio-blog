70d6a9888bad1e815de2d432c8995e1c76220330
Guilherme <guilherme_devz@hotmail.com>
2021-09-21 19:10:13 -0300

    UPDATE: removing the console log of api key

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -5,8 +5,6 @@ const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_TOKEN);
 
 function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(process.env.SENDGRID_TOKEN);
-
   const pattern =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
