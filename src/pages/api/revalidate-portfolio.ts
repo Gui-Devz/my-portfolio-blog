e0500490271ee6d43bd40ebca15e614fbbf67344
gzagari@brasilcash.com.br <gzagari@brasilcash.com.br>
2022-06-12 17:54:57 -0300

    feat: adjusting some css

--- src/pages/api/revalidate-portfolio.ts
+++ src/pages/api/revalidate-portfolio.ts
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   // Check for secret to confirm this is a valid request
-  if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
+  if (req.query.secret !== process.env.NEXT_PUBLIC_SECRET_TOKEN) {
     return res.status(401).json({ message: "Invalid token" });
   }
 
