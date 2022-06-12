f0eaa18688faed001ec8ffca2a2757ff8acfd0c1
gzagari@brasilcash.com.br <gzagari@brasilcash.com.br>
2022-06-12 18:23:32 -0300

    updating next version to latest

--- src/pages/api/revalidate-portfolio.ts
+++ src/pages/api/revalidate-portfolio.ts
@@ -1,10 +1,21 @@
 export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    res
+      .status(400)
+      .json({ error: "Invalid HTTP method. Only Post requests are allowed." });
+  }
+
   // Check for secret to confirm this is a valid request
   if (req.query.secret !== process.env.NEXT_PUBLIC_SECRET_TOKEN) {
     return res.status(401).json({ message: "Invalid token" });
   }
 
   try {
+    const body = req.body;
+    if (!body) {
+      return res.status(400).send("Bad request (no body)");
+    }
+
     await res.unstable_revalidate("/");
     return res.json({ revalidated: true });
   } catch (err) {
